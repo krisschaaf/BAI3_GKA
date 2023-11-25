c0ebac8b7f4b28c521cf19daab7b43339a7bd9ba
acx382 <Iryna.Trygub@haw-hamburg.de>
2023-11-25 13:28:10 +0100

    P2-6/Graph generator fertig stellen

--- Praktikum_2/Code/src/main/java/haw/gka/Main.java
+++ Praktikum_2/Code/src/main/java/haw/gka/Main.java
@@ -1,6 +1,19 @@
 package haw.gka;
+
+import haw.gka.io.GraphGenerator;
+import org.graphstream.graph.Graph;
+
+
 public class Main {
 	public static void main(String[] args) {
+
+		 GraphGenerator graphGenerator = new GraphGenerator();
+		 for (int i = 0; i < 1000; i++) {
+			 graphGenerator.generateGraph(40,70,10, false);
+			 System.out.println(i);
+		 }
+
+
 		System.setProperty("org.graphstream.ui", "swing");
 	}
 }
