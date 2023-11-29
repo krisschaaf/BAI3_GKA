f196ceafc09be696a9cb726b07d05385faf69813
krisschaaf <kris-schaaf@gmx.de>
2023-11-29 15:04:08 +0100

    fix compilation error

--- Praktikum_2/Code/src/test/java/haw/gka/kruskal/KruskalMockImpl.java
+++ Praktikum_2/Code/src/test/java/haw/gka/kruskal/KruskalMockImpl.java
@@ -1,11 +1,14 @@
 package haw.gka.kruskal;
 
+import org.graphstream.graph.Edge;
 import org.graphstream.graph.implementations.MultiGraph;
+
+import java.util.HashSet;
 
 public class KruskalMockImpl implements Kruskal {
 
     @Override
-    public DisjointSet createMinimalSpanningForrest(MultiGraph graph) {
+    public HashSet<Edge> createMinimalSpanningForrest(MultiGraph graph) {
         //TODO use if needed
         return null;
     }
