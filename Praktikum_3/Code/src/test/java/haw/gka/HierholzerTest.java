94e629436e99f115f1407a934b191e4f4f5e0d3c
ansgar deuschel <ansgar.deuschel@gmx.de>
2024-01-12 12:15:16 +0100

    one more testcase

--- Praktikum_3/Code/src/test/java/haw/gka/HierholzerTest.java
+++ Praktikum_3/Code/src/test/java/haw/gka/HierholzerTest.java
@@ -2,6 +2,7 @@ package haw.gka;
 import org.graphstream.graph.Edge;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.SingleGraph;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
@@ -12,8 +13,49 @@ import java.util.stream.Collectors;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HierholzerTest {
+
     @Test
-    public void testIdentifiedEulerGraph() throws Exception {
+    public void testSimpleEulerGraph(){
+        Graph test = new SingleGraph("test");
+		test.addNode("1");
+		test.addNode("2");
+		test.addNode("3");
+		test.addNode("4");
+		test.addNode("5");
+		test.addNode("6");
+		test.addNode("7");
+		test.addNode("8");
+		test.addNode("9");
+
+		test.addEdge("1", "1","2", false);
+		test.addEdge("2", "1","3", false);
+		test.addEdge("3", "1","7", false);
+		test.addEdge("4", "1","8", false);
+		test.addEdge("5", "2","3", false);
+		test.addEdge("6", "3","4", false);
+		test.addEdge("7", "3","7", false);
+		test.addEdge("8", "4","5", false);
+		test.addEdge("9", "4","7", false);
+		test.addEdge("10", "4","9", false);
+		test.addEdge("11", "5","9", false);
+		test.addEdge("12", "6","7", false);
+		test.addEdge("13", "6","9", false);
+		test.addEdge("14", "7","9", false);
+		test.addEdge("15", "7","8", false);
+
+        List<Graph> eulerCircles = Hierholzer.findEulerGraphs(test);
+
+        HashSet<Edge> collectedEdges = new HashSet<Edge>();
+        for (Graph g : eulerCircles){
+            for(Node n : g.nodes().collect(Collectors.toList())){
+                assertEquals((n.getDegree()%2),0);
+            }
+            collectedEdges.addAll(g.edges().collect(Collectors.toSet()));
+        }
+        assertEquals(test.getEdgeCount(),collectedEdges.size());
+    }
+    @Test
+    public void testGeneratedEulerGraph() throws Exception {
         Graph test = GraphGenerator.createEulerGraph(100, 4000, "test");
         List<Graph> eulerCircles = Hierholzer.findEulerGraphs(test);
 
