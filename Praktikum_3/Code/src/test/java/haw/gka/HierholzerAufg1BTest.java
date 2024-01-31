c7539c5becc4358894bc6083bb737e7542441874
krisschaaf <kris-schaaf@gmx.de>
2024-01-31 19:19:08 +0100

    P3-1 add final doc by ansgar

---
+++ Praktikum_3/Code/src/test/java/haw/gka/HierholzerAufg1BTest.java
@@ -0,0 +1,13 @@
+package haw.gka;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HierholzerAufg1BTest {
+
+    @Test
+    public void test() {
+        assertEquals()
+    }
+}
